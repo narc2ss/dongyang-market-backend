3c375998351780e5a69908bc0c4e79dd71835679
narc2ss <aksrb5149@gmail.com>
2020-08-20 12:46:32 +0900

    attached morgan

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as dotenv from "dotenv";
 import * as Koa from "koa";
 import * as Router from "koa-router";
 import api from "./api";
+import morgan = require("koa-morgan");
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
@@ -9,8 +10,9 @@ const PORT = process.env.PORT || 4000;
 const app = new Koa();
 const router = new Router();
 
-router.use("/api", api.routes());
-
+app.use(morgan("dev"));
 app.use(router.routes()).use(router.allowedMethods());
+
+router.use("/api", api.routes());
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
