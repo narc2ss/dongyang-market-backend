b846b9be0229a8fa4185b31ff0593c702a1fb471
narc2ss <aksrb5149@gmail.com>
2020-08-20 12:02:32 +0900

    dotenv setting

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
+import * as dotenv from "dotenv";
 import * as Koa from "koa";
+import * as Router from "koa-router";
+
+dotenv.config();
+
 const app = new Koa();
+const router = new Router();
 
 app.use(async (ctx: { body: string }, next: () => void) => {
   const msg: string = "dongyang market";
@@ -7,6 +13,12 @@ app.use(async (ctx: { body: string }, next: () => void) => {
   next();
 });
 
-app.listen(4000, () =>
+router.post("/api/auth/login", (ctx: Koa.Context) => {
+  ctx.body = "login";
+});
+
+app.use(router.routes());
+
+app.listen(process.env.PORT, () =>
   console.log("start koa server at http://localhost:4000")
 );
